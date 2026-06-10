b0a64881a3bab49796838957674d5dda88e53ab3
BookShelter330 <76z8j57d7h@privaterelay.appleid.com>
2026-06-10 17:30:41 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,7 @@ const urlsToCache = [
     './',
     './index.html',
     './style.css',
-    './app.js',
-    './manifest.json'
+    './app.js'
 ];
 
 self.addEventListener('install', event => {
@@ -18,19 +17,5 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => response || fetch(event.request))
-    );
-});
-
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
     );
 });
